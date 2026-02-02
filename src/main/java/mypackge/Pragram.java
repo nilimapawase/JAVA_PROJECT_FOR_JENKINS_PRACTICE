f2fe166fad5f75a73d4a84f3758eb282d40f62c2
Nilima Pawase <nilimapawase14@gmail.com>
2026-02-02 12:39:59 +0530

    Initial commit

---
+++ src/main/java/mypackge/Pragram.java
@@ -0,0 +1,19 @@
+package mypackge;
+
+public class Pragram {
+	
+	public void Add()
+	{
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Addition is : "+c);
+	}
+
+	public static void main(String[] args) {
+		Pragram p=new Pragram();
+		p.Add();
+		
+
+	}
+
+}
